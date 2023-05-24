a89682a965dc1b029bafb4d2d7d9a998cf4b12c8
Ramarti <raul.mf86@gmail.com>
2023-05-24 12:58:45 -0700

    Lint (#3)
    * style and lint tasks
    
    * build: ignore
    
    * build: lint and missing deppendencies
    
    * build: lint github action
    
    * build: install deps for lintint
    
    * build: github action lint fix node version
    
    * build: establish dependency
    
    ---------
    
    Co-authored-by: Raul <drmanhattan@MBP-de-Raul.fibertel.com.ar>
    Co-authored-by: Raul <raul@proofofcode.dev>

--- test/foundry/Counter.t.sol
+++ test/foundry/Counter.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../../contracts/Counter.sol";
+import { Test } from "forge-std/Test.sol";
+import { Counter } from "../../contracts/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
